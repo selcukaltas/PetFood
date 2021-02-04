42b90c4895b9600c12de8a0c2ad20b6c5fc17600
Selcuk Altas <altas.selcuk@outlook.com>
2021-02-04 13:13:49 +0300

    Interfaces Added

---
+++ src/ApplicationCore/Entities/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class OrderItem : BaseEntity
+    {
+        public decimal UnitPrice { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string PictureUri { get; set; }
+        public int Units { get; set; }
+    }
+}
